6392b5d105217686855cc194508eb619cd64b631
elnar <50706044213@KLASSA302-20>
2025-04-04 13:57:14 +0300

    20.03 tund

---
+++ KooliProjekt.WinFormsApp/Api/Building.cs
@@ -0,0 +1,9 @@
+﻿namespace KooliProjekt.WinFormsApp.Api
+{
+    public class Building
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
+    
---
+++ KooliProjekt.WinFormsApp/Api/IApiClient.cs
@@ -0,0 +1,9 @@
+﻿namespace KooliProjekt.WinFormsApp.Api
+{
+    public interface IApiClient
+    {
+        Task<Result<List<Building>>> List();
+        Task Save(Building list);
+        Task Delete(int id);
+    }
+}
